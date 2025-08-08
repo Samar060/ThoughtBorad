c090d0fc6053d441ebfd3537a73c43a4e0d4b4bc
rahul061203 <rs177621@gmail.com>
2025-08-09 00:42:59 +0530

    fixed the ratelimit issue and deploying

--- frontend/src/components/RateLimitedUI.jsx
+++ frontend/src/components/RateLimitedUI.jsx
@@ -1,8 +1,9 @@
 import { ZapIcon } from "lucide-react";
 
+
 const RateLimitedUi = () => {
   return (
-    <div className="max-w-6xl mx-auto px-4 py-8">
+     <div className="max-w-6xl mx-auto px-4 py-8">
       <div className="bg-primary/10 border border-primary/30 rounded-lg shadow-md">
         <div className="flex flex-col md:flex-row items-center p-6">
           <div className="flex-shrink-0 bg-primary/20 p-4 rounded-full mb-4 md:mb-0 md:mr-6">
@@ -20,7 +21,7 @@ const RateLimitedUi = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default RateLimitedUi;
