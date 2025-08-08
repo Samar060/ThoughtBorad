845f7434329d22c28e17b30ca25a82f44d88c361
rahul061203 <rs177621@gmail.com>
2025-08-09 00:32:58 +0530

    prepared again for deploymeny

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -40,7 +40,7 @@ const HomePage = () => {
     <div className="min-h-screen">
       <Navbar />
 
-      {isRateLimited && <RateLimitedUI />}
+      {isRateLimited && <RateLimitedUi />}
 
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
