b4a9f111b668cb2629a7900112a792babeea3dca
rahul061203 <rs177621@gmail.com>
2025-08-09 01:33:47 +0530

    again fixes the issue

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Navbar from "../components/Navbar";
-import RateLimitedUi from "../components/RateLimitedUi";
+import RateLimitedUI from "../components/RateLimitedUi";
 import { useEffect } from "react";
 import api from "../components/lib/axios";
 
@@ -40,7 +40,7 @@ const HomePage = () => {
     <div className="min-h-screen">
       <Navbar />
 
-      {isRateLimited && <RateLimitedUi/>}
+      {isRateLimited && <RateLimitedUI/>}
 
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
