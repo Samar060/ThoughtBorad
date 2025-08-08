7256d9c125926e13d9747bde8e4c794fcfcb8b80
rahul061203 <rs177621@gmail.com>
2025-08-09 01:40:32 +0530

    fixed issue

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Navbar from "../components/Navbar";
-import RateLimitedUI from "../components/RateLimitedUi";
+import RateLimitedUI from "../components/RateLimitedUI";
 import { useEffect } from "react";
 import api from "../components/lib/axios";
 
